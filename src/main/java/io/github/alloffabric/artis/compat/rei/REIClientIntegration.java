8245ffde2ec01985bc73d23109330098dd2b0e1e
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-02-17 16:17:15 +0100

    more rei

--- src/main/java/io/github/alloffabric/artis/compat/rei/REIClientIntegration.java
+++ src/main/java/io/github/alloffabric/artis/compat/rei/REIClientIntegration.java
@@ -1,21 +1,31 @@
 package io.github.alloffabric.artis.compat.rei;
 
+import dev.architectury.event.EventResult;
 import io.github.alloffabric.artis.Artis;
 import io.github.alloffabric.artis.api.ArtisCraftingRecipe;
 import io.github.alloffabric.artis.api.ArtisExistingItemType;
 import io.github.alloffabric.artis.api.ArtisTableType;
 import io.github.alloffabric.artis.block.ArtisTableBlock;
+import io.github.alloffabric.artis.inventory.ArtisCraftingScreen;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.display.visibility.DisplayVisibilityPredicate;
+import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.common.display.Display;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.common.BuiltinPlugin;
+import me.shedaniel.rei.plugin.common.DefaultPlugin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.registry.Registry;
 
 import java.util.HashMap;
@@ -50,7 +60,6 @@ public class REIClientIntegration implements REIClientPlugin {
                     registry.addWorkstations(BuiltinPlugin.CRAFTING, EntryStacks.of(block));
                 }
             }
-            
         }
     }
 
@@ -59,6 +68,25 @@ public class REIClientIntegration implements REIClientPlugin {
         for (ArtisTableType type : Artis.ARTIS_TABLE_TYPES) {
             registry.registerRecipeFiller(ArtisCraftingRecipe.class, type, ArtisRecipeDisplay::new);
         }
+        
+        /*registry.registerVisibilityPredicate(new DisplayVisibilityPredicate() {
+            @Override
+            public EventResult handleDisplay(DisplayCategory<?> category, Display display) {
+                if (display.getDisplayLocation().isPresent() && MinecraftClient.getInstance().world.getRecipeManager().get(display.getDisplayLocation().get()).isPresent()) {
+                    Recipe recipe = MinecraftClient.getInstance().world.getRecipeManager().get(display.getDisplayLocation().get()).get();
+        
+                    if (recipe.getType() instanceof ArtisTableType && display.getCategoryIdentifier().equals(DefaultPlugin.CRAFTING)) {
+                        return EventResult.interruptTrue();
+                    }
+                }
+                return EventResult.interruptFalse();
+            }
+    
+            @Override
+            public double getPriority() {
+                return 10;
+            }
+        });*/
     }
     
     /**
@@ -68,7 +96,6 @@ public class REIClientIntegration implements REIClientPlugin {
     @Override
     public void registerScreens(ScreenRegistry registry) {
         for (ArtisTableType type : Artis.ARTIS_TABLE_TYPES) {
-            // TODO: add screen container registration
             // since this one screen handles all different sizes there needs to be a better way to handle this
             //registry.registerContainerClickArea(type.getREIClickArea(), ArtisCraftingScreen.class, type.getCategoryIdentifier());
             //registry.registerContainerClickArea(type.getREIClickArea(), ArtisCraftingScreen.class, BuiltinPlugin.CRAFTING);
---
+++ src/main/java/io/github/alloffabric/artis/compat/rei/REIServerIntegration.java
@@ -0,0 +1,18 @@
+package io.github.alloffabric.artis.compat.rei;
+
+import io.github.alloffabric.artis.Artis;
+import io.github.alloffabric.artis.api.ArtisTableType;
+import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
+import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+
+public class REIServerIntegration implements REIServerPlugin {
+	
+	// For shift-clicking into crafting gui
+	@Override
+	public void registerDisplaySerializer(DisplaySerializerRegistry registry) {
+		for(ArtisTableType type : Artis.ARTIS_TABLE_TYPES) {
+			registry.register(type.getCategoryIdentifier(), ArtisRecipeDisplay.serializer());
+		}
+	}
+	
+}
