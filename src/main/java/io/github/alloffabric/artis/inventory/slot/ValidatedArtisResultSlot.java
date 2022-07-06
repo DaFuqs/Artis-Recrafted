c892989b7c70c8f987ee2cb9092d4bc0c742f68c
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-07-06 20:21:59 +0200

    catalysts work again

--- src/main/java/io/github/alloffabric/artis/inventory/slot/ValidatedArtisResultSlot.java
+++ src/main/java/io/github/alloffabric/artis/inventory/slot/ValidatedArtisResultSlot.java
@@ -62,27 +62,9 @@ public class ValidatedArtisResultSlot extends ValidatedSlot {
     @Override
     public void onTakeItem(PlayerEntity player, ItemStack stack) {
         this.onCrafted(stack);
+        
         DefaultedList<ItemStack> remainders = getRemainders();
-        for (int i = 0; i < remainders.size() - 1; ++i) {
-            ItemStack input = this.craftingInv.getStack(i);
-            ItemStack remainder = remainders.get(i);
-            if (!input.isEmpty()) {
-                this.craftingInv.removeStack(i, 1);
-                input = this.craftingInv.getStack(i);
-            }
-
-            if (!remainder.isEmpty()) {
-                if (input.isEmpty()) {
-                    this.craftingInv.setStack(i, remainder);
-                } else if (ItemStack.areItemsEqualIgnoreDamage(input, remainder) && ItemStack.areNbtEqual(input, remainder)) {
-                    remainder.increment(input.getCount());
-                    this.craftingInv.setStack(i, remainder);
-                } else if (!this.player.getInventory().insertStack(remainder)) {
-                    this.player.dropItem(remainder, false);
-                }
-            }
-        }
-
+        
         if (this.inventory instanceof RecipeUnlocker recipeUnlocker && recipeUnlocker.getLastRecipe() instanceof ArtisCraftingRecipe artisCraftingRecipe) {
             int catalystSlot = remainders.size() - 1;
             ItemStack remainder = remainders.get(catalystSlot).copy();
@@ -101,6 +83,26 @@ public class ValidatedArtisResultSlot extends ValidatedSlot {
                     catalyst.decrement(artisCraftingRecipe.getCatalystCost());
                 }
                 this.craftingInv.setStack(catalystSlot, catalyst);
+            }
+        }
+        
+        for (int i = 0; i < remainders.size() - 1; ++i) {
+            ItemStack input = this.craftingInv.getStack(i);
+            ItemStack remainder = remainders.get(i);
+            if (!input.isEmpty()) {
+                this.craftingInv.removeStack(i, 1);
+                input = this.craftingInv.getStack(i);
+            }
+
+            if (!remainder.isEmpty()) {
+                if (input.isEmpty()) {
+                    this.craftingInv.setStack(i, remainder);
+                } else if (ItemStack.areItemsEqualIgnoreDamage(input, remainder) && ItemStack.areNbtEqual(input, remainder)) {
+                    remainder.increment(input.getCount());
+                    this.craftingInv.setStack(i, remainder);
+                } else if (!this.player.getInventory().insertStack(remainder)) {
+                    this.player.dropItem(remainder, false);
+                }
             }
         }
     }
