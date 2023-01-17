db21e2ed1310d1dc6c6d01301b86836ebd66da59
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-01-17 18:07:15 +0100

    removed artis tooltip

--- src/main/java/de/dafuqs/artis/block/ArtisTableItem.java
+++ src/main/java/de/dafuqs/artis/block/ArtisTableItem.java
@@ -1,30 +1,11 @@
 package de.dafuqs.artis.block;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nullable;
-import java.util.List;
+import net.minecraft.item.*;
 
 public class ArtisTableItem extends BlockItem {
-    
+
     public ArtisTableItem(ArtisTableBlock block, Settings settings) {
         super(block, settings);
-    }
-
-    @Override
-    @Environment(EnvType.CLIENT)
-    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, @NotNull TooltipContext context) {
-        if (context.isAdvanced())
-            tooltip.add(new TranslatableText("tooltip.artis.source").formatted(Formatting.BLUE, Formatting.ITALIC));
     }
 
 }
